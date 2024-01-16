b13d7932a0599bf63582e4166df95d2bde2409bb
tldus2256 <you@example.com>
2024-01-16 10:38:20 +0900

    3장. 연산자

---
+++ src/ch04_control/Ex01_If.java
@@ -0,0 +1,25 @@
+package ch04_control;
+
+public class Ex01_If {
+
+	public static void main(String[] args) {
+		int score = 60 + (int) (41 * Math.random());	// 60 ~ 100
+		
+		if (score >= 80)
+			System.out.println(score + "점은 평균 이상입니다.");
+		else
+			System.out.println(score + "점은 평균 미만입니다.");
+	
+		char grade;
+		if (score >= 90)
+			grade = 'A';
+		else if (score >=80)
+			grade = 'B';
+		else if (score >= 70)
+			grade = 'C';
+		else
+			grade = 'D';
+		System.out.println("score:" + score + ", grade:" + grade);
+	}
+
+}
